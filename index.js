5b6df8f09a02f015cd330b6118d6365321346eb4
Your Name <you@example.com>
2022-09-23 11:08:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+        (function() {
+
+  var dropBtns = document.querySelectorAll('.headquestion');
+
+  function closeOpenItems() {
+      openMenus = document.querySelectorAll('.subtext');
+      openMenus.forEach(function(menus) {
+        menus.classList.remove('show');
+      });  
+  }
+
+  dropBtns.forEach(function(btn) {
+
+    btn.addEventListener('click', function(e) {
+      var 
+        dropContent = btn.querySelector('.subtext'),
+        shouldOpen = !dropContent.classList.contains('show');
+      e.preventDefault();
+
+        
+      closeOpenItems();
+      
+      if (shouldOpen) {
+        
+        dropContent.classList.add('show');   
+      //   for (i = 0; i < 6; i++)  {
+      //   document.querySelector("h2").style.fontWeight = "700";[i] 
+      //   console.log("suop")
+      //  }
+      }
+      e.stopPropagation();
+    });
+
+  });
+
+  window.addEventListener('click', function(event) {
+    if (event.target != dropBtns) {
+      closeOpenItems();
+    }
+  })
+
+  })();
+
+
+  //  document.querySelector("h2").style.fontWeight = "700"; 
+//     accItem.style.display = "block";
+//   } else {
+//     document.querySelector("h2").style.fontWeight = "400"; 
